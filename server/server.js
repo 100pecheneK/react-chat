a5c4f36c6eacfb20c13ee3a580a70dfb20ccd48e
100pecheneK <mistermihail23@gmail.com>
2020-12-12 17:05:26 +0500

    test

--- server/server.js
+++ server/server.js
@@ -14,6 +14,11 @@ const io = require('socket.io')(http, {
 })
 
 app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('/test', (req, res) => {
+  var list = ['item1', 'item2', 'item3']
+  res.json(list)
+  console.log('Sent list of items')
+})
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '../client/build/index.html'))
 })
